3b782859c96009837c8d1eb4960acf695f1daad2
dawitzeleke <dawitzeleke92@gmail.com>
2024-03-11 11:44:37 +0300

    Structure & Layout

---
+++ bank_modern_app/src/components/Navbar.jsx
@@ -0,0 +1,13 @@
+import {useState} from 'react'
+
+import {close, logo, menu} from '../assets';
+import {navLinks} from '../constants';
+
+
+function Navbar() {
+  return (
+    <div>Navbar</div>
+  )
+}
+
+export default Navbar
